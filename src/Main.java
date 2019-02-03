323ad8daa79aaeba7523b6d0e9ba87e4172b570c
grigoryoskin <44236259+grigoryoskin@users.noreply.github.com>
2019-02-04 00:13:17 +0900

    Update Main.java
    Added formatting

--- src/Main.java
+++ src/Main.java
@@ -7,72 +7,72 @@ import java.net.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-int port = 8080;
-ServerSocketChannel serverChannel;
-Selector selector;
-try {
-	serverChannel = ServerSocketChannel.open();
-	ServerSocket ss = serverChannel.socket();
-	InetSocketAddress address = new InetSocketAddress(port);
-	ss.bind(address);
-	serverChannel.configureBlocking(false);
-	selector = Selector.open();
-	serverChannel.register(selector, SelectionKey.OP_ACCEPT);
-	
-}catch(IOException ex) {
-	ex.printStackTrace();
-	return;
-}
-while (true) {
-try {
-selector.select();
-} catch (IOException ex) {
-ex.printStackTrace();
-break;
-}
-Set<SelectionKey> readyKeys = selector.selectedKeys();
-Iterator<SelectionKey> iterator = readyKeys.iterator();
-while (iterator.hasNext()) {
-SelectionKey key = iterator.next();
-iterator.remove();
-try {
-if (key.isAcceptable()) {
-ServerSocketChannel server = (ServerSocketChannel) key.channel();
-SocketChannel client = server.accept();
-System.out.println("Accepted connection from " + client);
-client.configureBlocking(false);
-SelectionKey clientKey = client.register(
-selector, SelectionKey.OP_WRITE | SelectionKey.OP_READ);
-ByteBuffer buffer = ByteBuffer.allocate(100);
-clientKey.attach(buffer);
-}
-if (key.isReadable()) {
-SocketChannel client = (SocketChannel) key.channel();
-ByteBuffer output = (ByteBuffer) key.attachment();
-client.read(output);
-}
-if (key.isWritable()) {
-SocketChannel client = (SocketChannel) key.channel();
-ByteBuffer output = (ByteBuffer) key.attachment();
-output.flip();
-client.write(output);
-output.compact();
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int port = 8080;
+        ServerSocketChannel serverChannel;
+        Selector selector;
+        try {
+            serverChannel = ServerSocketChannel.open();
+            ServerSocket ss = serverChannel.socket();
+            InetSocketAddress address = new InetSocketAddress(port);
+            ss.bind(address);
+            serverChannel.configureBlocking(false);
+            selector = Selector.open();
+            serverChannel.register(selector, SelectionKey.OP_ACCEPT);
 
-//
-}
-} catch (IOException ex) {
-key.cancel();
-try {
-key.channel().close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return;
+        }
+        while (true) {
+            try {
+                selector.select();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                break;
+            }
+            Set < SelectionKey > readyKeys = selector.selectedKeys();
+            Iterator < SelectionKey > iterator = readyKeys.iterator();
+            while (iterator.hasNext()) {
+                SelectionKey key = iterator.next();
+                iterator.remove();
+                try {
+                    if (key.isAcceptable()) {
+                        ServerSocketChannel server = (ServerSocketChannel) key.channel();
+                        SocketChannel client = server.accept();
+                        System.out.println("Accepted connection from " + client);
+                        client.configureBlocking(false);
+                        SelectionKey clientKey = client.register(
+                            selector, SelectionKey.OP_WRITE | SelectionKey.OP_READ);
+                        ByteBuffer buffer = ByteBuffer.allocate(100);
+                        clientKey.attach(buffer);
+                    }
+                    if (key.isReadable()) {
+                        SocketChannel client = (SocketChannel) key.channel();
+                        ByteBuffer output = (ByteBuffer) key.attachment();
+                        client.read(output);
+                    }
+                    if (key.isWritable()) {
+                        SocketChannel client = (SocketChannel) key.channel();
+                        ByteBuffer output = (ByteBuffer) key.attachment();
+                        output.flip();
+                        client.write(output);
+                        output.compact();
 
-} catch (IOException cex) {}
+                        //
+                    }
+                } catch (IOException ex) {
+                    key.cancel();
+                    try {
+                        key.channel().close();
 
-}
-}
+                    } catch (IOException cex) {}
 
-}
-	}
+                }
+            }
+
+        }
+    }
 
 }
